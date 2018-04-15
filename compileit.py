aa1ea1e93bf269ba2e000ee137e74857a96c0b0c
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2018-04-15 19:26:59 -0400

    default compileit to compile all src files

--- compileit.py
+++ compileit.py
@@ -11,21 +11,29 @@ os.chdir("build")
 
 detect_os = sys.platform;
 if detect_os == "linux":
-    if len(sys.argv) == 1:
-        pass  # TODO no args --> compile everything
+    if len(sys.argv) == 1:  # compile all src files if no args
+        os.chdir("../src")
+        src_files = [f for f in os.listdir() if os.path.isfile(f)]
+        os.chdir("../build")
+        for f in src_files:
+            if os.path.isfile("../src/" + f):
+                # create object files in build for argument source files
+                os.system("g++ -c -std=c++17 -g -O3 -Wall ../src/" + f);
+            else:
+                print("File " + f + " does not exist.")
     else:
         for arg in sys.argv[1:]:
             if os.path.isfile("../" + arg):
-                # create object files for argument source files
+                # create object files in build for argument source files
                 os.system("g++ -c -std=c++17 -g -O3 -Wall ../" + arg);
             else:
                 print("File " + arg + " does not exist.")
 
-        obj_files = [f for f in os.listdir() if os.path.isfile(f)]
-        compile_cmd = "g++ -std=c++17 -g -O3 -Wall -o ../exe "
-        for o in obj_files:
-            compile_cmd += o + " "
-        os.system(compile_cmd);
+    obj_files = [f for f in os.listdir() if os.path.isfile(f)]
+    compile_cmd = "g++ -std=c++17 -g -O3 -Wall -o ../exe "
+    for o in obj_files:
+        compile_cmd += o + " "
+    os.system(compile_cmd);
 else:
     print("Unsupported OS.")
 
