1512c105eff1db70cfd711f25f58308691f3e5a6
Maxwell Anderson <maxwell.brayden.anderson@gmail.com>
2018-04-18 11:23:16 -0400

    remove old compile script

--- compile_all.py
+++ compile_all.py
@@ -14,14 +14,14 @@ if detect_os == "linux":
     os.chdir("../src")
     src_files = [f for f in os.listdir() if os.path.isfile(f)]
     os.chdir("../build")
- 
+
     for f in src_files:
         if os.path.isfile("../src/" + f):
             # create object files in build for all source files
             os.system("g++ -c -std=c++17 -g -O3 -Wall ../src/" + f);
         else:
             print("File " + f + " does not exist.")
-
+ 
     obj_files = [f for f in os.listdir() if os.path.isfile(f)]
     compile_cmd = "g++ -std=c++17 -g -O3 -Wall -o ../exe "
     for o in obj_files:
