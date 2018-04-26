5ce5c275011d54991146835d33b0b8bfd839882a
LuckyChucky <luckychuckynorris@gmail.com>
2018-04-26 15:21:56 -0400

    switch to CMake

--- make.py
+++ make.py
@@ -1,39 +1,20 @@
 #!/usr/bin/env python
 #
 # make.py
-# 
+#
 # Maxwell Anderson 2018
 
-import os, sys
+import os
 
-try: 
+try:
     os.makedirs("build")
 except FileExistsError as e:
     pass
 
 os.chdir("build")
 
-detect_os = sys.platform;
-if detect_os == "linux":
-    flags = "-std=c++17 -g -O3 -Wall -Wextra"
+os.system("cmake ..")
 
-    os.chdir("../src")
-    src_files = [f for f in os.listdir() if os.path.isfile(f)]
-    os.chdir("../build")
-
-    for f in src_files:
-        if os.path.isfile("../src/" + f):
-            # create object files in build for all source files
-            os.system("g++ -c " + flags + " ../src/" + f);
-        else:
-            print("File " + f + " does not exist.")
- 
-    obj_files = [f for f in os.listdir() if os.path.isfile(f)]
-    compile_cmd = "g++ " + flags + " -o ../thekinggame.exe "
-    for o in obj_files:
-        compile_cmd += o + " "
-    os.system(compile_cmd);
-else:
-    print("Unsupported OS.")
+os.system("make")
 
 os.chdir("..")
